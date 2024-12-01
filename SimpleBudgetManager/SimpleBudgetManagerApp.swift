e114d074c3c377a762a2ab505252ebde40300e66
Simon <Simon>
2024-11-30 19:28:50 -0600

    Initial Commit

---
+++ SimpleBudgetManager/SimpleBudgetManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBudgetManagerApp.swift
+//  SimpleBudgetManager
+//
+//  Created by Simon Wilke on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBudgetManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleBudgetManagerTests/SimpleBudgetManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBudgetManagerTests.swift
+//  SimpleBudgetManagerTests
+//
+//  Created by Simon Wilke on 11/30/24.
+//
+
+import Testing
+@testable import SimpleBudgetManager
+
+struct SimpleBudgetManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
